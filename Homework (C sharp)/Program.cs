748bbc6c3121bde5ce115a8fb22e22512732148a
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-06-28 00:48:23 +0700

    Add project files.

---
+++ Homework (C sharp)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework__C_sharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

9fa5c60c8ace920850d66046761be69bdca4226e
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-07-01 03:16:48 +0700

    done

--- Homework 1/Program.cs
+++ Homework 1/Program.cs
@@ -18,7 +18,8 @@ namespace Homework_1
     {
         static void Main()
         {
-            char key;
+//ДЗ 1.  -------------------------------------------------------------------------------------------------------------------------------
+            /*char key;
             int count_space = 0;
             Console.Write("Введите символы: ");
             do
@@ -31,6 +32,112 @@ namespace Homework_1
             Console.WriteLine();
             Console.Write("Количество пробелов: ");
             Console.WriteLine($"{count_space}");
+//ДЗ 2.  -------------------------------------------------------------------------------------------------------------------------------
+            int down = 0;
+            int up = 0;
+            char symbolChar = ' ';
+            Console.WriteLine("Введите маленькие и большие буквы:");
+            while (symbolChar != '.')
+            {
+                var symbol = Console.Read();
+               
+                if (symbol >= 'A' && symbol <= 'Z' || symbol >= 'А' && symbol <= 'Я')
+                {
+                    symbol += 32;
+                    ++down;
+                }
+                else if (symbol >= 'a' && symbol <= 'z' || symbol >= 'а' && symbol <= 'я')
+                {
+                    symbol -= 32;
+                    ++up;
+                }
+                symbolChar = (char)symbol;
+                Console.Write(symbolChar);            
+            }
+            Console.WriteLine();
+            Console.Write("Количество измененных символов: ");
+            Console.WriteLine($"{down + up}");*/
+//ДЗ 3.  -------------------------------------------------------------------------------------------------------------------------------
+            /*int a, b;
+            Console.WriteLine("Введите числа A < B: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            while (a <= b)
+            {
+                for (int i = 0; i < a; i++)
+                    Console.Write($"{a}");
+                Console.WriteLine();
+                a++;
+            }*/
+//ДЗ 4.  ----------------------------------------------------------------------------------------------------------------------------
+            const int m = 8;
+            const int n = 6;
+            int[] arrayM = new int[m] { 2, 2, 4, 5, 6, 7, 8, 9 };
+            int[] arrayN = new int[n] { 2, 2, 4, 5, 6, 7 };
+            int size = 0;
+            RandomArray(arrayM);
+            PrintArray(arrayM);
+            RandomArray(arrayN);
+            PrintArray(arrayN);
+            for (int i = 0; i < m; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (arrayM[i] == arrayN[j]) size++;
+                }
+            }            
+            Console.Write("Размер выделяемой памяти для третьего массива: ");
+            Console.WriteLine(size);
+            int[] arrayC = new int[size];
+            int povtor = 0;
+            int it = 0;
+            
+            for (int i = 0; i < m; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (arrayM[i] == arrayN[j])
+                    {
+                        for (int k = 0; k < size; k++)
+                        {
+                            if (arrayC[k] == arrayN[j]) povtor++;
+                        }                       
+                        if (povtor == 0)
+                        {
+                            arrayC[it] = arrayN[j];
+                            it++;
+                        }
+                        povtor = 0;
+                    }
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{arrayC[i]}");
+            }
+            Console.WriteLine();
+
         }
+        static void RandomArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+        }
+        static void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+        }
+//ДЗ 5.  ----------------------------------------------------------------------------------------------------------------------------
     }
 }
